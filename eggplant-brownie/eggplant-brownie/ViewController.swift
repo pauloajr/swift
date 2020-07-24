944347fab8b56e0e30d928aaf3724f881153fc70
Paulo Junior <juniorestudante@hotmail.com>
2020-07-23 23:53:42 -0300

    Colocando componentes na tela

---
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by juninho on 7/24/20.
+//  Copyright © 2020 juninho lube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
