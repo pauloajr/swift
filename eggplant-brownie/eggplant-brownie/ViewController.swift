bd76a8a4299787f3b4a24a94e04e401d83629703
Paulo Junior <juniorestudante@hotmail.com>
2020-07-24 00:32:56 -0300

    Adicionando action

--- eggplant-brownie/eggplant-brownie/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBAction func adicionar(_ sender: Any) {
+        print("adicionar --")
     }
-
-
+    
 }
 
