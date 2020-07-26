b988fb2e31c96b5ac16d91be59167d1ff83fb3c0
Paulo Junior <juniorestudante@hotmail.com>
2020-07-26 00:36:09 -0300

    Trabalhando com array e lacos for

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -44,3 +44,58 @@ func alimentoConsumidoParametroStr(_ nome: String, _ peso: Double){
 alimentoConsumidoParametro(anome: "namorada", apeso: caloria)
 alimentoConsumidoParametroStr(namorada, caloria)
 
+
+
+let calorias = [50.5, 90, 120 ,20]
+for umacaloria in calorias {
+    print(umacaloria)
+}
+
+let notas = [9.5, 9, 8.6]
+print(notas)
+
+//totalDeCalorias.count-1
+print("totalDeCalorias.count - serve", notas.count)
+var soma: Double = 0
+/*for i in 0...totalDeCalorias.count-1 {
+    soma += (notas[i] * Double(i+1))
+    print(notas[i])
+}*/
+soma += notas[0] * 3
+soma += notas[1] * 2
+soma += notas [2] * 2
+soma = soma / 7
+print(soma)
+
+/*
+ 
+ for caloria in totalDeCalorias {
+     print(caloria)
+ }
+ 
+ */
+
+func calcularSomaDasNotaDaJulia(_ notas: Array<Double>){
+    var somar: Double = 0
+    for nota in notas{
+        somar += nota
+    }
+    print(somar)
+}
+
+calcularSomaDasNotaDaJulia(notas)
+
+func calcularSomaDasNotasDaJuliaComRetorno(_ notas: [Double]) -> Double {
+    var somar: Double = 0
+    for nota in notas{
+        somar += nota
+    }
+    return somar
+}
+
+soma = calcularSomaDasNotasDaJuliaComRetorno(notas)
+print("A média calculada com o sistema aritmética ", soma)
+
+// Array<>, Array<Double>, [Double], Array<???>
+
+// Explicita Array<Double>, implicita [Double]
